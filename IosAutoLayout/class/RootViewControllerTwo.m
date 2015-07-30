a98b2e7f257cd69007ceed1e3f1dfeef521f1021
zdsoft <11@qq.com>
2015-07-30 16:54:43 +0800

    layout

--- IosAutoLayout/class/RootViewControllerTwo.m
+++ IosAutoLayout/class/RootViewControllerTwo.m
@@ -9,15 +9,58 @@
 #import "RootViewControllerTwo.h"
 
 @interface RootViewControllerTwo ()
-
+@property (strong, nonatomic) UIButton *btn;
 @end
 
+
 @implementation RootViewControllerTwo
+@synthesize btn;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self initTopBar:[NSDictionary dictionaryWithObjectsAndKeys:@"第2个", @"topBarTitle", nil]];
+    
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:view];
+    [view setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    NSDictionary *dic = NSDictionaryOfVariableBindings(view);
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[view(80)]" options:0 metrics:nil views:dic]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[view(80)]" options:0 metrics:nil views:dic]];
+    
+    
+    btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [btn setTitle:@"msg" forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor greenColor];
+    //禁止自动转换AutoresizingMask
+    [btn setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.view addSubview:btn];
+    
+    //居中
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:btn attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+    
+    
+    //距离底部20单位
+    //注意NSLayoutConstraint创建的constant是加在toItem参数的，所以需要-20。
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:btn attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1 constant:-200]];
+    
+    ////定义高度是父View的三分之一
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:btn attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeHeight multiplier:0.3 constant:0]];
+    
+    
+    //注册kvo
+    [btn addObserver:self forKeyPath:@"bounds" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionInitial context:nil];
+    
+}
+
+
+//kvo回调
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
+    if(object == btn && [keyPath isEqualToString:@"bounds"]){
+         [btn setTitle:NSStringFromCGSize(btn.bounds.size) forState:UIControlStateNormal];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
